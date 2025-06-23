a4079b862ce59b2bc805e9e8d0a7d5595d6c6f18
05nashitian <120276003+05nashitian@users.noreply.github.com>
2025-06-23 19:12:14 +0800

    Update Run.java

--- src/main/java/top/srcrs/Run.java
+++ src/main/java/top/srcrs/Run.java
@@ -2,18 +2,12 @@ package top.srcrs;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import top.srcrs.domain.Cookie;
 import top.srcrs.util.Encryption;
 import top.srcrs.util.Request;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -24,253 +18,161 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * 程序运行开始的地方
- *
- * @author srcrs
- * @Time 2020-10-31
  */
 public class Run {
-    /**
-     * 获取日志记录器对象
-     */
     private static final Logger LOGGER = LoggerFactory.getLogger(Run.class);
 
-    /**
-     * 获取用户所有关注贴吧
-     */
-    String LIKE_URL = "https://tieba.baidu.com/mo/q/newmoindex";
-    /**
-     * 获取用户的tbs
-     */
-    String TBS_URL = "http://tieba.baidu.com/dc/common/tbs";
-    /**
-     * 贴吧签到接口
-     */
-    String SIGN_URL = "http://c.tieba.baidu.com/c/c/forum/sign";
+    String LIKE_URL  = "https://tieba.baidu.com/mo/q/newmoindex";
+    String TBS_URL   = "http://tieba.baidu.com/dc/common/tbs";
+    String SIGN_URL  = "http://c.tieba.baidu.com/c/c/forum/sign";
 
-    /**
-     * 存储用户所关注的贴吧
-     */
-    private List<String> follow = new ArrayList<>();
-    /**
-     * 签到成功的贴吧列表
-     */
+    private List<String> follow  = new ArrayList<>();
     private static List<String> success = new ArrayList<>();
-
-    /**
-     * 签到失败的贴吧列表
-     */
-    private static HashSet<String> failed = new HashSet<String>();
-
-    /**
-     * 失效的贴吧列表
-     */
+    private static Set<String>  failed  = new HashSet<>();
     private static List<String> invalid = new ArrayList<>();
-
-    /**
-     * 用户的tbs
-     */
     private String tbs = "";
-    /**
-     * 用户所关注的贴吧数量
-     */
-    private static Integer followNum = 201;
+    private static Integer followNum = 0;
 
     public static void main(String[] args) {
-        Cookie cookie = Cookie.getInstance();
-        // 存入Cookie，以备使用
+        // 1. 从 args[0] 读取 BDUSS
         if (args.length == 0) {
-            LOGGER.warn("请在Secrets中填写BDUSS");
+            LOGGER.warn("请在运行参数中填写 BDUSS");
+            return;
         }
+        Cookie cookie = Cookie.getInstance();
         cookie.setBDUSS(args[0]);
+
         Run run = new Run();
+        // 2. 获取 tbs
         run.getTbs();
+        // 3. 获取关注贴吧列表
         run.getFollow();
+        followNum = followNum == 0 ? follow.size() + invalid.size() : followNum;
+        // 4. 执行签到
         run.runSign();
-        LOGGER.info("共 {} 个贴吧 - 成功: {} - 失败: {} - {} ", followNum, success.size(), followNum - success.size(), failed);
-        LOGGER.info("失效 {} 个贴吧: {} ", invalid.size(), invalid);
+
+        // 5. 日志输出
+        LOGGER.info("共 {} 个贴吧 - 成功: {} - 失败: {}", followNum, success.size(), followNum - success.size());
+        LOGGER.info("失效 {} 个贴吧: {}", invalid.size(), invalid);
+
+        // 6. 原有 PushPlus 通知（如果使用）
         if (args.length == 2) {
             run.send(args[1]);
         }
+
+        // 7. 新增：Telegram Bot 通知
+        String tgToken  = System.getenv("TELEGRAM_BOT_TOKEN");
+        String tgChatId = System.getenv("TELEGRAM_CHAT_ID");
+        if (tgToken != null && tgChatId != null) {
+            // 构建摘要
+            String text = "贴吧签到结果：\n" +
+                          "总: " + followNum + "，成功: " + success.size() + "，失败: " + (followNum - success.size());
+            String desp = "成功列表: " + success + "\n" +
+                          "失败列表: " + failed + "\n" +
+                          "失效列表: " + invalid;
+            run.sendTelegram(tgToken, tgChatId, text + "\n\n" + desp);
+        } else {
+            LOGGER.warn("未配置 TELEGRAM_BOT_TOKEN/TELEGRAM_CHAT_ID，跳过 Telegram 通知");
+        }
     }
 
-    /**
-     * 进行登录，获得 tbs ，签到的时候需要用到这个参数
-     *
-     * @author srcrs
-     * @Time 2020-10-31
-     */
     public void getTbs() {
         try {
             JSONObject jsonObject = Request.get(TBS_URL);
             if ("1".equals(jsonObject.getString("is_login"))) {
-                LOGGER.info("获取tbs成功");
+                LOGGER.info("获取 tbs 成功");
                 tbs = jsonObject.getString("tbs");
             } else {
-                LOGGER.warn("获取tbs失败 -- " + jsonObject);
+                LOGGER.warn("获取 tbs 失败 -- " + jsonObject);
             }
         } catch (Exception e) {
-            LOGGER.error("获取tbs部分出现错误 -- " + e);
+            LOGGER.error("获取 tbs 部分出现错误 -- ", e);
         }
     }
 
-    /**
-     * 获取用户所关注的贴吧列表
-     *
-     * @author srcrs
-     * @Time 2020-10-31
-     */
     public void getFollow() {
         try {
             JSONObject jsonObject = Request.get(LIKE_URL);
             LOGGER.info("获取贴吧列表成功");
-            JSONArray jsonArray = jsonObject.getJSONObject("data").getJSONArray("like_forum");
-            followNum = jsonArray.size();
-            // 获取用户所有关注的贴吧
-            for (Object array : jsonArray) {
-                String tiebaName = ((JSONObject) array).getString("forum_name");
-                if ("0".equals(((JSONObject) array).getString("is_sign"))) {
-                    // 将为签到的贴吧加入到 follow 中，待签到
-                    follow.add(tiebaName.replace("+", "%2B"));
-                    // 过滤失效的贴吧
-                    if (Request.isTiebaNotExist(tiebaName)) {
-                        follow.remove(tiebaName);
-                        invalid.add(tiebaName);
-                        failed.add(tiebaName);
+            JSONArray list = jsonObject.getJSONObject("data").getJSONArray("like_forum");
+            followNum = list.size();
+            for (Object o : list) {
+                JSONObject jo = (JSONObject) o;
+                String forum = jo.getString("forum_name");
+                if ("0".equals(jo.getString("is_sign"))) {
+                    String enc = URLEncoder.encode(forum, "UTF-8");
+                    if (Request.isTiebaNotExist(forum)) {
+                        invalid.add(forum);
+                        failed.add(forum);
+                    } else {
+                        follow.add(enc);
                     }
                 } else {
-                    // 将已经成功签到的贴吧，加入到 success
-                    success.add(tiebaName);
+                    success.add(forum);
                 }
             }
         } catch (Exception e) {
-            LOGGER.error("获取贴吧列表部分出现错误 -- " + e);
+            LOGGER.error("获取关注贴吧出现错误 -- ", e);
         }
     }
 
-    /**
-     * 开始进行签到，每一轮性将所有未签到的贴吧进行签到，一共进行5轮，如果还未签到完就立即结束
-     * 一般一次只会有少数的贴吧未能完成签到，为了减少接口访问次数，每一轮签到完等待1分钟，如果在过程中所有贴吧签到完则结束。
-     *
-     * @author srcrs
-     * @Time 2020-10-31
-     */
     public void runSign() {
-        // 当执行 5 轮所有贴吧还未签到成功就结束操作
-        Integer flag = 5;
+        int rounds = 5;
         try {
-            while (success.size() < followNum && flag > 0) {
-                LOGGER.info("-----第 {} 轮签到开始-----", 5 - flag + 1);
-                LOGGER.info("还剩 {} 贴吧需要签到", followNum - success.size());
-                Iterator<String> iterator = follow.iterator();
-                while (iterator.hasNext()) {
-                    String s = iterator.next();
-                    String rotation = s.replace("%2B", "+");
-                    String body = "kw=" + s + "&tbs=" + tbs + "&sign=" + Encryption.enCodeMd5("kw=" + rotation + "tbs=" + tbs + "tiebaclient!!!");
-                    JSONObject post = new JSONObject();
-                    post = Request.post(SIGN_URL, body);
-                    int randomTime = new Random().nextInt(200) + 300;
-                    LOGGER.info("等待 {} 毫秒", randomTime);
-                    TimeUnit.MILLISECONDS.sleep(randomTime);
-                    if ("0".equals(post.getString("error_code"))) {
-                        iterator.remove();
-                        success.add(rotation);
-                        failed.remove(rotation);
-                        LOGGER.info(rotation + ": " + "签到成功");
+            while (success.size() < followNum && rounds-- > 0) {
+                LOGGER.info("----- 第 {} 轮签到开始 -----", 6 - rounds);
+                for (Iterator<String> it = follow.iterator(); it.hasNext(); ) {
+                    String enc = it.next();
+                    String forum = URLDecoder.decode(enc, "UTF-8");
+                    String body = "kw=" + enc +
+                                  "&tbs=" + tbs +
+                                  "&sign=" + Encryption.enCodeMd5("kw=" + forum + "tbs=" + tbs + "tiebaclient!!!");
+                    JSONObject res = Request.post(SIGN_URL, body);
+                    TimeUnit.MILLISECONDS.sleep(new Random().nextInt(200) + 300);
+                    if ("0".equals(res.getString("error_code"))) {
+                        it.remove();
+                        success.add(forum);
+                        failed.remove(forum);
+                        LOGGER.info("{}: 签到成功", forum);
                     } else {
-                        failed.add(rotation);
-                        LOGGER.warn(rotation + ": " + "签到失败");
+                        failed.add(forum);
+                        LOGGER.warn("{}: 签到失败 -- {}", forum, res);
                     }
                 }
-                if (success.size() != followNum - invalid.size()) {
-                    // 为防止短时间内多次请求接口，触发风控，设置每一轮签到完等待 5 分钟
-                    Thread.sleep(1000 * 60 * 5);
-                    /**
-                     * 重新获取 tbs
-                     * 尝试解决以前第 1 次签到失败，剩余 4 次循环都会失败的错误。
-                     */
+                if (success.size() < followNum - invalid.size()) {
+                    Thread.sleep(5 * 60 * 1000);
                     getTbs();
                 }
-                flag--;
             }
         } catch (Exception e) {
-            LOGGER.error("签到部分出现错误 -- " + e);
+            LOGGER.error("签到过程中出现错误 -- ", e);
         }
     }
 
+    /** 原有 PushPlus 发送方法略 */
+
     /**
-     * 发送运行结果到微信，通过 server 酱
-     *
-     * @param sckey
-     * @author srcrs
-     * @Time 2020-10-31
+     * 使用 Telegram Bot API 推送消息
      */
-    /**   public void send(String sckey) {
-       
-        String text = "总: " + followNum + " - ";
-        text += "成功: " + success.size() + " 失败: " + (followNum - success.size());
-        String desp = "共 " + followNum + " 贴吧\n\n";
-        desp += "成功: " + success.size() + " 失败: " + (followNum - success.size());
-        String body = "text=" + text + "&desp=" + "TiebaSignIn运行结果\n\n" + desp;
-        StringEntity entityBody = new StringEntity(body, "UTF-8");
-        HttpClient client = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost("https://sc.ftqq.com/" + sckey + ".send");
-        httpPost.addHeader("Content-Type", "application/x-www-form-urlencoded");
-        httpPost.setEntity(entityBody);
-        HttpResponse resp = null;
-        String respContent = null;
+    public void sendTelegram(String botToken, String chatId, String message) {
         try {
-            resp = client.execute(httpPost);
-            HttpEntity entity = null;
-            if (resp.getStatusLine().getStatusCode() < 400) {
-                entity = resp.getEntity();
-            } else {
-                entity = resp.getEntity();
+            String encoded = URLEncoder.encode(message, "UTF-8");
+            String url = String.format(
+                "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s&disable_web_page_preview=true",
+                botToken, chatId, encoded
+            );
+            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+            conn.setRequestMethod("GET");
+            try (BufferedReader in = new BufferedReader(
+                        new InputStreamReader(conn.getInputStream(), "UTF-8"))) {
+                String line;
+                while ((line = in.readLine()) != null) {
+                    // 可选：打印 Telegram 返回内容
+                    System.out.println("Telegram 返回: " + line);
+                }
             }
-            respContent = EntityUtils.toString(entity, "UTF-8");
-            LOGGER.info("server酱推送正常");
         } catch (Exception e) {
-            LOGGER.error("server酱发送失败 -- " + e);
-        }
-    } 
-**/
-      /**
-     * 发送运行结果到微信，通过 PUSHPLUS
-     *
-     * @param sckey
-     * @author srcrs
-     * @Time 2020-10-31
-     */
-     public void send(String sckey) {
-        /** 将要推送的数据 */
-        String text = "总: " + followNum + " - ";
-        text += "成功: " + success.size() + " 失败: " + (followNum - success.size());
-        String desp = "共 " + followNum + " 贴吧\n\n";
-        desp += "成功: " + success.size() + " 失败: " + (followNum - success.size());
-        String body = "text=" + text + "&desp=" + "TiebaSignIn运行结果\n\n" + desp;
-
-try {
-            String token = sckey;
-            String title = URLEncoder.encode("百度贴吧自动签到", "UTF-8");
-            String content = URLEncoder.encode(desp, "UTF-8");
-            String urlx = "https://www.pushplus.plus/send?title=" + title + "&content=" + content + "&token=" + token;
-            URL url = new URL(urlx);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String line;
-            StringBuilder response = new StringBuilder();
-
-            while ((line = reader.readLine()) != null) {
-                response.append(line);
-            }
-            reader.close();
-
-            System.out.println("Response: " + response.toString());
-            connection.disconnect();
-
-        } catch (Exception e) {
-            e.printStackTrace();
+            LOGGER.error("Telegram 推送失败 -- ", e);
         }
     }
 }
